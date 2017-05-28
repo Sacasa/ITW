f875a3a482e4ad10f30cba60bbc37e69aaaf5200
Sacasa <mrsacasagaming@gmail.com>
2017-05-28 13:53:03 +0200

    màj slider

---
+++ SITE/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
